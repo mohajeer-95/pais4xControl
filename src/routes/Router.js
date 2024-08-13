12d719b3ad2278eb7c684252a293958bf7960322
eska-developer <eskadenia.developer@gmail.com>
2024-08-13 17:38:04 +0300

    to hash

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -1,5 +1,5 @@
 import { Suspense, lazy } from "react";
-import { BrowserRouter as Router, Route, Routes, Navigate } from "react-router-dom";
+import { HashRouter as Router, Route, Routes, Navigate } from "react-router-dom";
 
 // Lazy load the components
 const FullLayout = lazy(() => import("../layouts/FullLayout.js"));
