b9691b2f6fba5ab77c39bd57e8501a85bde8e9da
eska-developer <eskadenia.developer@gmail.com>
2024-08-13 17:31:48 +0300

    with baseName

--- src/routes/Router.js
+++ src/routes/Router.js
@@ -16,7 +16,7 @@ const Breadcrumbs = lazy(() => import("../views/ui/Breadcrumbs"));
 
 const AppRouter = ({ isAuthenticated, handleLogin, handleLogout }) => {
   return (
-    <Router>
+    <Router basename="/pais4xcontrol">
       <Suspense fallback={<div>Loading...</div>}>
         <Routes>
           <Route
