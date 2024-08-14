63ba6d31ae07de2dd67680eed45e78abec77f5a8
eska-developer <eskadenia.developer@gmail.com>
2024-08-14 20:13:08 +0300

    solve slider delete

--- src/views/ui/Slider.js
+++ src/views/ui/Slider.js
@@ -35,7 +35,7 @@ const SliderList = () => {
     }
   };
 
-  const handleDelete = async (id) => {
+  const handleDeleteOLD = async (id) => {
     const myHeaders = new Headers();
     const token = 'eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiIxIiwianRpIjoiMjg2MDllNzQ0OTgxZjMwNzIyZTllNmRlMDQyMjY2MDJmNDM0NTk3ODBlNzUzM2FiNDc3MGFhMjhiZDFjZjRhMjU5NGNhYjI3MGJhM2UxNzUiLCJpYXQiOjE3MTc2MjA2NDQsIm5iZiI6MTcxNzYyMDY0NCwiZXhwIjoxNzQ5MTU2NjQ0LCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.cRRWjwpsvG5cYb-5n_YpfrOHfoWzRTEoYndZffA08MrJxWnv4KZ1mezT6PaKxPdDNEnRQaeqPSdo4Wtf5xWrlgpZTnNQmd8-_xXjz_LJHIXdGtT5YosL6If__d1psx6nsW8ckyXx2mKWF2hiM6a1c65keOgtmiEwCsADxBYZv-VAE1eZz8eQHaYmc_dBFZnfevZqaDOcYjSdijRBGXYGCb65-wolO08is5cf6jq-r6m0pWKQ1Gwq8h5BRV4HBqxLVJK-2Mbk2hHF0EsxrUSIE9wTKxqBcH0SL4wOA8GWeiQUSlAncTkwvGQwFBdGVMou67XDz5UeiN0ek6JayemFZcpq-fkHP7l1fNpT_6SgQSRaIxca2rx9Q_nQYrjIczzuvPchbs9MbOdKdEpuolOWAbI4VBGS0FvMiXF596LCLuyAnnAC2YppkMF6AepxMBWtvCpYgL_Kr6KK4e-39_7tzhe1nMZsIRg-2jEYkHv8TB1vr4VaN4IikA6gEVlfml51VW6aiej2YkY4WtdJSeVAOhPgcPYX396v7awfW7liQz4TOIS0sxSaFTTlCrKvqxBz676uzmqxq0oUqTBm_4vlAirm1iluAXP9NwxYKHxIF8SNHaKtsBvMIzjaN79pBHmQdvRc_mJRf-Z9y5Wzn0-m6LmM2c60WA0Hu-vquTPZmbc'; // Your actual token
     myHeaders.append('Authorization', `Bearer ${token}`);
@@ -57,6 +57,35 @@ const SliderList = () => {
       console.error('Error deleting slider:', error);
     }
   };
+
+
+  const handleDelete = async (sliderId) => {
+    const myHeaders = new Headers();
+    myHeaders.append("Content-Type", "application/json");
+    myHeaders.append("Authorization", "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiJ9.eyJhdWQiOiI0IiwianRpIjoiMzQyMDZhYzM2NmM4OTVmYjM5ODZhZDA4MGNlZjFjZTg2MTliZjRlMTJiYTUxOWQwNjE2ZDNlYzdkMDVkOTk4YzY2M2VkY2UyNmJmYWRkMTkiLCJpYXQiOjE3MjM2NTUxMzcsIm5iZiI6MTcyMzY1NTEzNywiZXhwIjoxNzU1MTkxMTM3LCJzdWIiOiIyIiwic2NvcGVzIjpbXX0.MllXJn2VVbF1Szt2YLi4s4fu1AqliQAJEL9ely_oOSuh8HYgEvQGUe9Cwi4QeRHma721lzbMRBzObdgE_UCsC07m9uY_dMy27ChgS7NQnmEiOLLv1AwX70ctJh9tJQZY6ERZvn-ajZqooPX_D9V11saQL7hbFfbW0kpyeaLFqnrxaRQvLeZ2o79AuXUQpHPD_1bVnQbipesyrgoxYOAXhw48vMu8-rp_KDhwcEo6r1ONmVikm8pTq-3zsp0R-Li0oZW7wqjOZG0ODJCVm4jEF9erEptVaI4dX5F6bcrxQ0QlRGbvVIR00WKwkgF3-pPadmvavLOmv_6SnodoZmobojOermpkJSAGKqwH08zzOA1c0pu9WgXHT2CjSWRvvgPuHuEAEwmr1TBiVpixZFQEsp0d8TYNi_vzDSzj9vISDanhg0tnsLHSY1X0pJhXqGoBZxAO-bH6IsIVZ_qvh0_W2f39AtBCvv3qYDGgD2D8jH4PIA8MqiFo47gmjAl-DA-cO8fqSLAqhzTX6w9voGRTXC1OI69inPL6cBRge3i8HyqmqDBowt0E8TMMNAqfmAqpGED1Wj1yGfiM8ofUCWsWC_mnhZvsz7xoYFv1g57cBXRZTcwTl5RmsBbNc1EFh0QM8GdNhijWV1OXqc2RQfYteM1KcbveQgKTF17KSqBjwzI");
+  
+    const raw = JSON.stringify({
+      id: sliderId,
+    });
+  
+    const requestOptions = {
+      method: "DELETE",
+      headers: myHeaders,
+      body: raw,
+      redirect: "follow",
+    };
+  
+    try {
+      const response = await fetch(`http://lab.app2serve.com/public/api/slider/${sliderId}`, requestOptions);
+      const result = await response.text();
+      console.log(result);
+      fetchSliders();
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  };
+
 
   const handleInputChange = (e) => {
     const { name, value } = e.target;
